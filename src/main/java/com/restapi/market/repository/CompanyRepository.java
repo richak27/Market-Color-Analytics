877105c183b646d20fa0a3b52d925ff93809eaac
richak27 <richakapuskari27@gmail.com>
2020-05-29 20:34:28 +0530

    added findbysector

--- src/main/java/com/restapi/market/repository/CompanyRepository.java
+++ src/main/java/com/restapi/market/repository/CompanyRepository.java
@@ -1,5 +1,7 @@
 package com.restapi.market.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import com.restapi.market.model.Company;
 @Repository
 public interface CompanyRepository extends MongoRepository<Company, String> {
 	Company findByTicker(String ticker);
+	List<Company> findBySector(String sector);
 
 	
 }
