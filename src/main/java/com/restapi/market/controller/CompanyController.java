120098744c854a09c23aa28263a48ab7ed15a91e
ShreyaChivilkar <47733871+ShreyaChivilkar@users.noreply.github.com>
2020-05-30 01:30:30 +0530

    Average Calculation

--- src/main/java/com/restapi/market/controller/CompanyController.java
+++ src/main/java/com/restapi/market/controller/CompanyController.java
@@ -3,22 +3,34 @@ package com.restapi.market.controller;
 import java.text.ParseException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.restapi.market.model.Company;
+import com.restapi.market.model.PriceAverage;
+import com.restapi.market.model.Stock;
+import com.restapi.market.model.VolumeAverage;
 import com.restapi.market.service.CompanyService;
 
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}	
+	
+	
 	@Autowired
 	private CompanyService companyService;
 
 	@GetMapping("/{ticker}")
-	public Company getCompany(@PathVariable("ticker") String ticker) {
+	public Company getCompany(@PathVariable("ticker") String ticker) throws ParseException{
 		return companyService.getByTicker(ticker);
 	}
 
@@ -33,7 +45,7 @@ public class CompanyController {
 	}
 
 	@GetMapping("/update/{ticker}")
-	public String updateByTicker(@PathVariable("ticker") String ticker) throws ParseException {
+	public String updateByTicker(@PathVariable("ticker") String ticker)throws ParseException  {
 		return companyService.updateByTicker(ticker);
 	}
 
@@ -47,4 +59,17 @@ public class CompanyController {
 		return companyService.addStocksByTicker(ticker);
 	}
 
+	
+	@GetMapping("/average-volume/{ticker}")
+	public VolumeAverage calAverageVolume(@PathVariable("ticker") String ticker) {
+		Company company = companyService.getByTicker(ticker);
+		return companyService.calAverageVolume(company);
+	}
+	
+	@GetMapping("/average-stock/{ticker}")
+	public PriceAverage calAverageStock(@PathVariable("ticker") String ticker) {
+		Company company = companyService.getByTicker(ticker);
+		return companyService.calAverageStock(company);
+	}
+	
 }
