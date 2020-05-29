7056edb1761b37c64f854242421f49e10ca9f2f5
ShreyaChivilkar <47733871+ShreyaChivilkar@users.noreply.github.com>
2020-05-29 14:29:24 +0530

    Calculate Average Stock and Volume for a company based on Ticker

--- CompanyController.java
+++ CompanyController.java
@@ -1,106 +1,56 @@
+package com.restapi.market.controller;
 
-package com.example.consumer;
-
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-
-	
-
-@RestController
-@RequestMapping("/company")
-public class CompanyController {
-	
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-	
-	
-	@Autowired
-	private CompanyService companyService;	
-
-	@GetMapping("/{ticker}")
-    public Company getCompany(@PathVariable("ticker") String ticker) {       
-		return companyService.getByTicker(ticker);        
-    }	
-	
-	@GetMapping("/tickers")
-    public Set<String> getTickerList() {       
-		return companyService.getAllTickers();        
-    }
-	
-	@GetMapping("/seed")
-    public String populateDb() {		
-		return companyService.seedDb();
-    }
-}
-
-
-
-/*
-
-package com.example.consumer;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import com.restapi.market.model.Company;
+import com.restapi.market.service.CompanyService;
+
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
-	
-	
 	@Autowired
 	private CompanyService companyService;
 
-
-	private List<String> list;
-	private List<Company> obj;
-
-
 	@GetMapping("/{ticker}")
-    public Company getByTicker(@PathVariable("ticker") String ticker){
+	public Company getCompany(@PathVariable("ticker") String ticker) {
+		return companyService.getByTicker(ticker);
+	}
 
+	@GetMapping("/tickers")
+	public List<String> getTickerList() {
+		return companyService.getAllTickers();
+	}
 
-       return companyService.getByTicker(ticker);
+	@GetMapping("/seed")
+	public String populateDb() {
+		return companyService.seedDb();
+	}
 
-    public Company getByTicker(@PathVariable("ticker") String ticker){ 
-       return companyService.getByTicker(ticker);      
-    }
+	@GetMapping("/update/{ticker}")
+	public String updateByTicker(@PathVariable("ticker") String ticker) {
+		return companyService.updateByTicker(ticker);
+	}
 
+	@GetMapping("/force-update")
+	public void forceUpdate() {
+		companyService.dailyUpdateAll();
+	}
+	
+	@GetMapping("/average-volume/{ticker}")
+	public float calAverageVol(@PathVariable("ticker") String ticker) {
+		Company company = companyService.getByTicker(ticker);
+		return companyService.calAverageVol(company);
+	}
+	
+	@GetMapping("/average-stock/{ticker}")
+	public float calAverageStock(@PathVariable("ticker") String ticker) {
+		Company company = companyService.getByTicker(ticker);
+		return companyService.calAverageStock(company);
+	}
 
 }
-	@GetMapping("/ticker")
-	public List<String> getAllTicker(){	
-		 list = companyService.getAllTickers();
-			return companyService.getAllTickers();
-		}
-
-
-	@GetMapping("/ticker/stock")
-	public List<Company> getCompanyFromTicker(List<String>list){
-
-		return this.companyService.getCompanyFromTicker(list = companyService.getAllTickers());		
-
-		}
-
-	@GetMapping("/sector")
-	public List<String> getAllSector(){	
-		 list = companyService.getAllSectors();
-			return companyService.getAllSectors();
-		}
-
-}	*/
