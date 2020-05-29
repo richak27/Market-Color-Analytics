1425f76c6126f74a5d6fa3d63863ad9fe37a523e
ShreyaChivilkar <47733871+ShreyaChivilkar@users.noreply.github.com>
2020-05-29 14:37:53 +0530

    Changed Annotations

--- CompanyController.java
+++ CompanyController.java
@@ -2,10 +2,12 @@ package com.restapi.market.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.restapi.market.model.Company;
 import com.restapi.market.service.CompanyService;
@@ -13,6 +15,13 @@ import com.restapi.market.service.CompanyService;
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}	
+	
+	
 	@Autowired
 	private CompanyService companyService;
 
