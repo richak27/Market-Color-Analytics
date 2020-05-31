9552d700510cce15ef232cc0618c8201d7148483
richak27 <richakapuskari27@gmail.com>
2020-05-31 14:34:31 +0530

    calling of findBy method moved to service

--- src/main/java/com/restapi/market/controller/CompanyController.java
+++ src/main/java/com/restapi/market/controller/CompanyController.java
@@ -2,6 +2,8 @@ package com.restapi.market.controller;
 
 import java.text.ParseException;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.restapi.market.model.Company;
-import com.restapi.market.model.Stock;
-
 import com.restapi.market.model.PriceAverage;
 import com.restapi.market.model.VolumeAverage;
 import com.restapi.market.service.CompanyService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/company")
@@ -30,53 +31,35 @@ public class CompanyController {
 	private CompanyService companyService;
 
 	@GetMapping("/{ticker}")
+	@CrossOrigin(origins = "http://localhost:51535")
 	public Company getCompany(@PathVariable("ticker") String ticker) throws ParseException {
 		return companyService.getByTicker(ticker);
 	}
 
 	@GetMapping("/tickers")
+	@CrossOrigin(origins = "http://localhost:51535")
 	public List<String> getTickerList() {
 		return companyService.getAllTickers();
 	}
 
 	@GetMapping("/seed")
+	@CrossOrigin(origins = "http://localhost:51535")
 	public String populateDb() {
 		return companyService.seedDb();
 	}
 
 	@GetMapping("/update/{ticker}")
+	@CrossOrigin(origins = "http://localhost:51535")
 	public String updateByTicker(@PathVariable("ticker") String ticker) throws ParseException {
 		return companyService.updateByTicker(ticker);
 	}
 
 	@GetMapping("/force-update")
+	@CrossOrigin(origins = "http://localhost:51535")
 	public void forceUpdate() {
 		companyService.dailyUpdateAll();
 	}
-	
-	@GetMapping("/getVolumeByCompany{ticker}")
-	public float GetAvgVolumeByCompany(@PathVariable("ticker") String ticker)
-	{
-		Company company = companyService.getByTicker(ticker);
-		return companyService.getAvgVolumeByCompany(company);
-	} 
 
-
-	@GetMapping("/getStockByCompany/{ticker}")
-	public float GetAvgStockByCompany(@PathVariable("ticker") String ticker)
-	{
-		Company company = companyService.getByTicker(ticker);
-		return companyService.getAvgStockByCompany(company);
-	}
-	
-	
-	@GetMapping("/getStockBySector/{sector}")
-	public float getvalue(@PathVariable("sector") String sector)
-	{
-		List<Company> company = companyService.getBySector(sector);
-		return companyService. getAvgStockBySector(company);
-	}
-	
 	@GetMapping("/add/{ticker}")
 	public String addStocksByTicker(@PathVariable("ticker") String ticker) throws ParseException {
 		return companyService.addStocksByTicker(ticker);
@@ -84,28 +67,52 @@ public class CompanyController {
 
 
 	@GetMapping("/average-volume-company/{ticker}")
+	@CrossOrigin(origins = "http://localhost:51535")
 	public VolumeAverage calAverageVolume(@PathVariable("ticker") String ticker) {
-		Company company = companyService.getByTicker(ticker);
-		return companyService.calAvgVolByCompany(company);
+		return companyService.calAvgVolByCompany(ticker);
 	}
 	
-	@GetMapping("/average-stock-company/{ticker}")
-	public PriceAverage calAvgStockByCompany(@PathVariable("ticker") String ticker) {
-		Company company = companyService.getByTicker(ticker);
-		return companyService.calAvgStockByCompany(company);
+	@GetMapping("/average-price-company/{ticker}")
+	@CrossOrigin(origins = "http://localhost:51535")	
+	public PriceAverage calAvgPriceByCompany(@PathVariable("ticker") String ticker) {
+		return companyService.calAvgPriceByCompany(ticker);
 	}
 	
-	@GetMapping("/average-stock-sector/{sector}")
+	@GetMapping("/average-price-sector/{sector}")
+	@CrossOrigin(origins = "http://localhost:51535")
 	public PriceAverage calAvgStockBySector(@PathVariable("sector") String sector) {
-		List<Company> company = companyService.getBySector(sector);
-		return companyService.calAvgStockBySector(company);
+		return companyService.calAvgPriceBySector(sector);
 	}
 	
 	@GetMapping("/average-volume-sector/{sector}")
-	public VolumeAverage calAvgVolumekBySector(@PathVariable("sector") String sector) {
-		List<Company> company = companyService.getBySector(sector);
-		return companyService.getAvgVolumekBySector(company);
+	@CrossOrigin(origins = "http://localhost:51535")
+	public VolumeAverage calAvgVolumeBySector(@PathVariable("sector") String sector) {
+		return companyService.calAvgVolumeBySector(sector);
 	}
 	
 
+	@GetMapping("/volume-deviation-sector")
+	@CrossOrigin(origins = "http://localhost:51535")
+	public Map<String, Double> getSectorVolumeDeviation() {
+		return companyService.getSectorVolumeDeviation();
+	}
+
+	@GetMapping("/price-deviation-sector")
+	@CrossOrigin(origins = "http://localhost:51535")
+	public Map<String, Double> getSectorPriceDeviation() {
+		return companyService.getSectorPriceDeviation();
+	}
+
+	@GetMapping("/volume-deviation-company")
+	@CrossOrigin(origins = "http://localhost:51535")
+	public Map<String, Double> getCompanyVolumeDeviation() {
+		return companyService.getCompanyVolumeDeviation();
+	}
+
+	@GetMapping("/price-deviation-company")
+	@CrossOrigin(origins = "http://localhost:51535")
+	public Map<String, Double> getCompanyPriceDeviation() {
+		return companyService.getCompanyPriceDeviation();
+	}
+	
 }
