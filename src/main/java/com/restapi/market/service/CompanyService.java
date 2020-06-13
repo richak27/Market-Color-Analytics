5263a79f23f0bc55ea20c6face32890d00019091
ShreyaChivilkar <shreyasc1999@gmail.com>
2020-06-13 22:05:52 +0530

    Merged Endpoints

--- src/main/java/com/restapi/market/service/CompanyService.java
+++ src/main/java/com/restapi/market/service/CompanyService.java
@@ -453,35 +453,6 @@ public class CompanyService {
 		return objList;
 	}
 
-	// List of objects with daily data for a sector
-	public List<List<DailyData>> gridSector(String sector, String startDate, String endDate) throws ParseException {
-		List<Company> companies = getBySector(sector);
-		List<List<DailyData>> nestedList = new ArrayList<>();
-		for (Company company : companies) {
-			nestedList.add(gridCompany(company.getTicker(), startDate, endDate));
-		}
-		return nestedList;
-	}
-
-	// Companies with sector selected, returns only companies----p
-	public Map<String, List<Double>> chartCompanySector(List<String> tickerList, List<String> sectorList, String type,
-			String boundaryDate) throws ParseException {
-
-		Map<String, List<Double>> dataMap = new HashMap<>();
-		for (String ticker : tickerList) {
-			Company company = getByTicker(ticker);
-			if (sectorList.contains(company.getSector())) {
-
-				AverageValues obj = companyAverage(ticker, type, boundaryDate);
-				dataMap.put(company.getName(), Arrays.asList(obj.getPreCovidValue(), obj.getPostCovidValue()));
-			}
-
-		}
-
-		return dataMap;
-	}
-
-
 
 	// grid grid grid main
 	public List<DailyData> getGridData(String startDate, String endDate, List<String> gotTickers,
