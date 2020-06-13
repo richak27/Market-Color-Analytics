d1351db22ab0ed69be0110f4873ef3e3d97dd501
Hrishikesh Rajasekharan <45428343+HrishikeshRajasekharan@users.noreply.github.com>
2020-06-13 16:15:43 +0530

    Removed code smells

--- src/main/java/com/restapi/market/model/DailyData.java
+++ src/main/java/com/restapi/market/model/DailyData.java
@@ -1,7 +1,7 @@
 package com.restapi.market.model;
 
-public class DailyData implements Comparable<DailyData>{
-	
+public class DailyData implements Comparable<DailyData> {
+
 	public DailyData(String companyName, String sector, String ticker, String price, String volume, String date) {
 		super();
 		this.companyName = companyName;
@@ -11,56 +11,67 @@ public class DailyData implements Comparable<DailyData>{
 		this.volume = volume;
 		this.date = date;
 	}
-	
-	
-	public DailyData() {};
-	
+
+	public DailyData() {
+		super();
+	}
+
 	private String companyName;
 	private String sector;
 	private String ticker;
 	private String price;
 	private String volume;
 	private String date;
-	
-	
+
 	public String getPrice() {
 		return price;
 	}
+
 	public String getVolume() {
 		return volume;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setPrice(String price) {
 		this.price = price;
 	}
+
 	public void setVolume(String volume) {
 		this.volume = volume;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
-	
+
 	public String getCompanyName() {
 		return companyName;
 	}
+
 	public String getSector() {
 		return sector;
 	}
+
 	public String getTicker() {
 		return ticker;
 	}
+
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
+
 	public void setSector(String sector) {
 		this.sector = sector;
 	}
+
 	public void setTicker(String ticker) {
 		this.ticker = ticker;
 	}
 
+	
 
 	@Override
 	public int compareTo(DailyData o) {
