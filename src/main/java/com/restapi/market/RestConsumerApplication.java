7ec9adcf6bea3de1fb82beb4725e5adf567ff1b1
Hrishikesh Rajasekharan <45428343+HrishikeshRajasekharan@users.noreply.github.com>
2020-06-16 19:05:28 +0530

    Update RestConsumerApplication.java

--- src/main/java/com/restapi/market/RestConsumerApplication.java
+++ src/main/java/com/restapi/market/RestConsumerApplication.java
@@ -3,15 +3,12 @@ package com.restapi.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableScheduling
 public class RestConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestConsumerApplication.class, args);
 	}
-
 }
