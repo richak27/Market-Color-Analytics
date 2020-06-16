b5606741b05c1b600e22cb93108a6db97bde7862
Hrishikesh Rajasekharan <45428343+HrishikeshRajasekharan@users.noreply.github.com>
2020-06-16 20:02:51 +0530

    Update CompanyController.java

--- src/main/java/com/restapi/market/controller/CompanyController.java
+++ src/main/java/com/restapi/market/controller/CompanyController.java
@@ -5,13 +5,11 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.restapi.market.model.ChartObjectCustom;
 import com.restapi.market.model.DailyData;
