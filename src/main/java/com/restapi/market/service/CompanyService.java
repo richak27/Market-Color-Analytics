f835ceca64d4d44c424cf346882d33a933c3d733
Hrishikesh Rajasekharan <45428343+HrishikeshRajasekharan@users.noreply.github.com>
2020-06-23 13:43:54 +0530

    Rectified average

--- src/main/java/com/restapi/market/service/CompanyService.java
+++ src/main/java/com/restapi/market/service/CompanyService.java
@@ -610,13 +610,13 @@ public class CompanyService  {
 		List<String> monthLabel = new ArrayList<>();
 		List<String> monthlabel = new ArrayList<>();
 		List<String> dayLabel = new ArrayList<>();
-		List<String> daylabel = new ArrayList<>();
-		List<Stock> stocknew = new ArrayList<>();
+		List<String> daylabel = new ArrayList<>();	
 		
 		List<Integer> weekLabel = new ArrayList<>();
 		List<Integer> weeklabel = new ArrayList<>();
 		
 		for (String ticker : tickerList) {
+			List<Stock> stocknew = new ArrayList<>();
 			List<Double>dev = new ArrayList<>();
 			ArrayList<Double> valueList = new ArrayList<>();
 
@@ -798,13 +798,14 @@ public class CompanyService  {
 		List<String> labels = new ArrayList<>();
 		List<String> dayLabel = new ArrayList<>();
 		List<String> daylabel = new ArrayList<>();
-		List<Stock> stocknew = new ArrayList<>();
+		
 		
 		List<Integer> weekLabel = new ArrayList<>();
 		List<Integer> weeklabel = new ArrayList<>();
 		
 
 		for (String sector : sectorList) {
+			List<Stock> stocknew = new ArrayList<>();
 			ArrayList<Double> valueList = new ArrayList<>();
 			List<Double>dev = new ArrayList<>();
 			List<Company> company = getBySector(sector);
